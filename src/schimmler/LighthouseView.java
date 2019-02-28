9365ce7766a88bddfff69819f020fdbea470cde1
nordisch93 <nordisch93@gmail.com>
2019-02-28 13:59:20 +0100

    added LighthouseView, this time for real

---
+++ src/schimmler/LighthouseView.java
@@ -0,0 +1,21 @@
+package schimmler;
+
+public class LighthouseView extends View {
+
+	public LighthouseView(Model model) {
+		super(model);
+	}
+
+	@Override
+	public int getClickedBlock(int x, int y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
